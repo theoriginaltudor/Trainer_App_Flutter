63704063fbb7394c311b1dd9e852d66dc4b0d867
theoriginaltudor <tudo0055@stud.kea.dk>
2019-09-17 10:45:23 +0200

    Update workouts.dart
    Added datepicker to the copy previous button

--- lib/screens/workout/workouts.dart
+++ lib/screens/workout/workouts.dart
@@ -11,13 +11,24 @@ class Workouts extends StatelessWidget {
       body: ListView(
         children: [
           CustomFlatButton(labelTitle: 'New Workout'),
-          CustomFlatButton(labelTitle: 'Copy previous'),
+          CustomFlatButton(labelTitle: 'Copy previous', _onTap: openDatePicker),
           ...cards(cardTitles)
         ],
       ),
     );
   }
-
+  
+  openDatePicker() {
+    DatePicker.showDatePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(2019, 9, 1),
+                              maxTime: DateTime.now(), onChanged: (date) {
+                            print('change $date');
+                          }, onConfirm: (date) {
+                                // navigate to training page with the workout from the chosen date
+                            print('confirm $date');
+                          }, currentTime: DateTime.now(), locale: LocaleType.zh);
+  }
   List<Widget> cards(List<String> cardTitles) {
     return cardTitles.map((title) => CustomCard()).toList();
   }
