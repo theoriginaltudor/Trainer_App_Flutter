47203468263d24d03d201f0dbe706929b94fe706
Tudor Caseru <theoriginaltudor@gmail.com>
2019-10-14 09:31:47 +0200

    added controller and onchnage function to custom text field

--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String labelTitle;
+  final TextEditingController controller;
+  final Function onChange;
 
-  CustomTextField({@required this.labelTitle});
+  CustomTextField({this.labelTitle, this.controller, this.onChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: TextField(
+        child: controller == null ? 
+        TextField(
           obscureText: false,
+          onEditingComplete: () => {this.onChange()},
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: labelTitle,
+          ),
+        ) :
+        TextField(
+          obscureText: false,
+          controller: controller,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: labelTitle,
